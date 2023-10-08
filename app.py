16902a70c14de4ddc8ad891d20570c8371101313
Hyunjae Kim <khj99present@gmail.com>
2023-10-08 17:20:43 +0900

    refactor: 애플리케이션 팩토리

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from flask_cors import CORS
 
-app = Flask(__name__)
+from views import create_endpoint
 
+class Services:
+    pass
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_app(test_config = None):
+    app = Flask(__name__)
 
+    CORS(app)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(app.config['DB_URL'], encoding="utf-8", max_overflow=0)
+
+    ## Presistence Layer
+
+    ## Business Layer
+    services = Services
+
+    ## endpoint 생성
+    create_endpoint(app, services)
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
