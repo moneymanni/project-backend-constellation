b3fd5b648531563cde5b0108c1d35a2796cc114d
Hyunjae Kim <khj99present@gmail.com>
2023-10-13 19:20:10 +0900

    refactor: endpoint 수정

--- views/index_view.py
+++ views/index_view.py
@@ -1,6 +1,10 @@
+from flask import jsonify
+
+from data import response_from_message, ResponseText, IndexMessage
+
 def create_endpoint(app, services):
 
     # health check endpoint
     @app.route('/ping', methods=['GET'])
     def ping():
-        return 'pong'
+        return jsonify(response_from_message(ResponseText.FAIL.value, IndexMessage.SUCCESS.value, 'pong')), 200
