7fe4a7dccc055c8c43cbd78785d477bd7f88fae6
Hyunjae Kim <khj99present@gmail.com>
2023-10-14 10:16:39 +0900

    refactor: 시각화 관련 endpoint 수정

--- views/visualization_view.py
+++ views/visualization_view.py
@@ -1,31 +1,62 @@
 from flask import Blueprint, request, jsonify
 
+from data import response_from_message, ResponseText, VisualizationMessage, PageMessage, LinkMessage
 
 def create_visualization_endpoint(services):
     visualization_view = Blueprint('visualization_view', __name__)
 
     link_service = services.link_service
     page_service = services.page_service
-    # jwt_service = services.jwt_service
+    note_service = services.note_service
+    jwt_service = services.jwt_service
 
     # node
+    @jwt_service.login_required
+    @note_service.confirm_auth
     @visualization_view.route('/node', methods=['GET'])
     def node():
         note_id = request.args.get('noteId')
 
-        return jsonify({
-            'note_id': note_id,
-            'node': page_service.find_page_id_and_keyword(note_id)
-        })
+        try:
+            node_list = page_service.find_page_id_and_keyword(note_id)
+
+            if isinstance(node_list, PageMessage):
+                message = response_from_message(ResponseText.FAIL.value, node_list.value)
+                return jsonify(message), 500
+        except Exception as e:
+            return jsonify(response_from_message(ResponseText.FAIL.value, VisualizationMessage.ERROR.value)), 500
+
+        return jsonify(response_from_message(ResponseText.SUCCESS.value, VisualizationMessage.READ.value, {
+            [{
+                'pageId': node['page_id'],
+                'keyword': node['keyword']
+            } for node in node_list]
+        })), 200
 
     # edge
+    @jwt_service.login_required
+    @note_service.confirm_auth
     @visualization_view.route('/edge', methods=['GET'])
     def edge():
         note_id = request.args.get('noteId')
 
-        return jsonify({
-            'note_id': note_id,
-            'edge': link_service.get_link_list_in_note(note_id)
-        })
+        try:
+            edge_list = link_service.get_link_list_in_note(note_id)
+
+            if isinstance(edge_list, LinkMessage):
+                message = response_from_message(ResponseText.FAIL.value, edge_list.value)
+                return jsonify(message), 500
+        except Exception as e:
+            return jsonify(response_from_message(ResponseText.FAIL.value, VisualizationMessage.ERROR.value)), 500
+
+        return jsonify(response_from_message(ResponseText.SUCCESS.value, VisualizationMessage.READ.value, {
+            [{
+                'pageId': edge['page_id'],
+                'linkedPageId': edge['linked_page_id'],
+                'linkage': edge['linkage'],
+                'createdAt': edge['created_at']
+            } for edge in edge_list]
+        })), 200
+
 
     return visualization_view
